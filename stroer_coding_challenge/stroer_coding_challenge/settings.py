5f6515c7bb473c0e68be46cbaf0b24afdef6c2ef
raLaaaa <r_ala@hotmail.de>
2023-06-15 13:32:59 +0200

    Update settings.py
    Set postresql as primary db

--- stroer_coding_challenge/stroer_coding_challenge/settings.py
+++ stroer_coding_challenge/stroer_coding_challenge/settings.py
@@ -83,23 +83,23 @@ WSGI_APPLICATION = "stroer_coding_challenge.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
-
 # DATABASES = {
 #     "default": {
-#         "ENGINE": "django.db.backends.postgresql",
-#         "NAME": "stroer",
-#         "USER": "user",
-#         "PASSWORD": "1234",
-#         "HOST": "database",
-#         "PORT": "5432",
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
 #     }
 # }
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "stroer",
+        "USER": "user",
+        "PASSWORD": "1234",
+        "HOST": "database",
+        "PORT": "5432",
+    }
+}
 
 
 # Password validation
